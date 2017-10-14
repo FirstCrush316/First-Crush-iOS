43de6fa686003e37aed6f63748327860af64f663
Sumit Johri <sumitjohri@Sumits-Mac.local>
2017-10-14 10:46:57 -0700

    Spinner works finally!! :)

--- First Crush/ViewController.swift
+++ First Crush/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController,UIWebViewDelegate,UIScrollViewDelegate {
-    @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var loadSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var navigationTitle: UINavigationItem!
-   
+    @IBOutlet weak var myLabel: UILabel!
     var lastOffsetY :CGFloat = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Removes it:
-        webView.delegate = self
+        self.webView.delegate = self
         webView.scrollView.delegate = self
         view.addSubview(webView)
         let url = URL(string: "http://www.firstcrush.co")
@@ -29,10 +29,12 @@ class ViewController: UIViewController,UIWebViewDelegate,UIScrollViewDelegate {
     
     func webViewDidStartLoad(_ : UIWebView) {
         loadSpinner.startAnimating()
+        myLabel.isHidden=false;
     }
     
     func webViewDidFinishLoad(_ : UIWebView) {
         loadSpinner.stopAnimating()
+        myLabel.isHidden=false;
     }
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView){
