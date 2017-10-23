bea9e42c192d8fac0d64cd20e5f70b0183cff3f8
firstcrushhome <31099808+firstcrushhome@users.noreply.github.com>
2017-10-23 12:01:06 -0700

    Progress bar & network handling implemented. :)

--- First Crush/ViewController.swift
+++ First Crush/ViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController,UIWebViewDelegate,UIScrollViewDelegate {
-    @IBOutlet weak var loadSpinner: UIActivityIndicatorView!
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var navigationTitle: UINavigationItem!
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var myLabel: UILabel!
     var lastOffsetY :CGFloat = 0
+    
+    var time : Float = 0.0
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +27,56 @@ class ViewController: UIViewController,UIWebViewDelegate,UIScrollViewDelegate {
         view.addSubview(webView)
         let url = URL(string: "http://www.firstcrush.co")
         let request = URLRequest(url: url!)
+        if Reachability.isConnectedToNetwork() == true {
             webView.loadRequest(request)
+             timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(ViewController.setProgress), userInfo: nil, repeats: true)
+        } else {
+            let alertController = UIAlertController(title: NSLocalizedString("No Internet Connection",comment:""), message: NSLocalizedString("Please ensure your device is connected to the internet.",comment:""), preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: NSLocalizedString("Ok", comment: ""), style: .default, handler: { (pAlert) in
+                //Do whatever you wants here
+            })
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+}
+    func setProgress() {
+        time += 0.1
+        progressView.progress = time / 5
+        if time >= 5 {
+            timer!.invalidate()
+        }
+    }
+    func webViewDidStartLoad(_ : UIWebView) {
+       self.progressView.setProgress(0.1, animated: true)
+        myLabel.isHidden=false
     }
     
-    func webViewDidStartLoad(_ : UIWebView) {
-        loadSpinner.startAnimating()
-        myLabel.isHidden=false;
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5, animations: { () -> Void in
+        self.progressView.setProgress(1.0, animated: true)
+            })
+
     }
     
     func webViewDidFinishLoad(_ : UIWebView) {
-        loadSpinner.stopAnimating()
-        myLabel.isHidden=false;
+       self.progressView.setProgress(1.0, animated: true)
+        myLabel.isHidden=true
+        self.progressView.isHidden=true
+    }
+    
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        self.progressView.setProgress(1.0, animated: true)
+        if Reachability.isConnectedToNetwork() == false {
+        let alertController = UIAlertController(title: NSLocalizedString("No Internet Connection",comment:""), message: NSLocalizedString("Please ensure your device is connected to the internet.",comment:""), preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: NSLocalizedString("Ok", comment: ""), style: .default, handler: { (pAlert) in
+            //Do whatever you wants here
+        })
+        alertController.addAction(defaultAction)
+        self.present(alertController, animated: true, completion: nil)
+        }
     }
     
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView){
